a5ede123efec5c67ab4dc6ae77c6c6df45303838
Anton Chernikov <avchernikov@8bitov.com>
2018-02-19 12:06:48 +0300

    Correct combinations function

--- combinations.php
+++ combinations.php
@@ -2,31 +2,32 @@
 
 function getCombinations(array $items, $k, $combinations = null)
 {
-    if ($k == 1) {
-        return $items;
-    }
-
     if ($combinations === null) {
         $combinations = $items;
     }
 
+    if ($k == 1) {
+        return $combinations;
+    }
+
     $newCombinations = [];
-    foreach ($combinations as $combination) {
+    foreach ($combinations as &$combination) {
+        if (!is_array($combination)) {
+            $combination = [$combination];
+        }
+
         foreach ($items as $item) {
-            if (!is_array($combination)) {
-                $combination = [$combination];
+            $newComb = array_merge($combination, [$item]);
+            sort($newComb);
+            if (!in_array($item, $combination) && !in_array($newComb, $newCombinations)) {
+                $newCombinations[] = $newComb;
             }
-
-            if (!in_array($item, $combination)) {
-                $combination[] = $item;
-                $newCombinations[] = $combination;
-            }
-
-
         }
     }
 
-    var_dump($newCombinations); die;
+    $k--;
 
-    return $combinations;
+    return getCombinations($items, $k, $newCombinations);
 }
+
+// usage example: getCombinations([1, 2, 3], 2);
