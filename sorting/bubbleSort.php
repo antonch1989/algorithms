3ab1d15438d043fb1b3cb98c37c7358695f3dc52
Anton Chernikov <avchernikov@8bitov.com>
2018-02-20 10:40:19 +0300

    bubble sort

---
+++ sorting/bubbleSort.php
@@ -0,0 +1,15 @@
+<?php
+
+function bubbleSort(array $arr) {
+    for ($i = 0; $i < count($arr) - 1; $i++) {
+        for ($j = $i + 1; $j < count($arr); $j++) {
+            if ($arr[$i] > $arr[$j]) {
+                $tmp = $arr[$i];
+                $arr[$i] = $arr[$j];
+                $arr[$j] = $tmp;
+            }
+        }
+    }
+
+    return $arr;
+}
